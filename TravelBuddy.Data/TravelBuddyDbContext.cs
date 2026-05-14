74278debb5ca0ba6e6086298e48ffc580d84d738
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-05-14 16:20:16 +0300

    Initial database created.

---
+++ TravelBuddy.Data/TravelBuddyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using TravelBuddy.Data.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelBuddy.Data
+{
+    public class TravelBuddyDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public TravelBuddyDbContext(DbContextOptions<TravelBuddyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
