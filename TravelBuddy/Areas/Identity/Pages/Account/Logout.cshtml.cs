4c95f82ede91faa39082f1a08e0908177913d532
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-05-14 17:08:51 +0300

    Cleaned up additional redundant code and usings.

--- TravelBuddy/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ TravelBuddy/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,14 +1,6 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using TravelBuddy.Data;
 using TravelBuddy.Data.Models;
 
 namespace TravelBuddy.Areas.Identity.Pages.Account;
